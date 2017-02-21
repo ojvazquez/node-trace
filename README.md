507e9dc3486020c8f55f1533c396f26e1dbe88c2
Walberto Ibarra <waldo.ibarra92@gmail.com>
2017-02-21 15:53:51 -0600

    Add optional object on warn and info

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # node-trace
 
+[![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg?style=flat-square)](https://github.com/Flet/semistandard)
+
 Simple node tracing library (ES6) for logging to stdout, stderr and JSON files using [bunyan](https://github.com/trentm/node-bunyan).
 
 ## Instalation
@@ -28,15 +30,19 @@ try {
 
 > All functions on class Trace are **static**, meaning you that should not instance it.
 
-### Trace.info(msg)
+### Trace.info([fields], msg)
+
+`fields`: Object (optional), merged into the log record.
 
 `msg`: String, message to log on *info.json* file.
 
-### Trace.warn(msg)
+### Trace.warn([fields], msg)
+
+`fields`: Object (optional), merged into the log record.
 
 `msg`: String, message to log on *warn.json* file.
 
-### Trace.error(err, msg)
+### Trace.error([err], msg)
 
 `err`: Object (optional), exception stack.
 
--- index.js
+++ index.js
@@ -1,80 +1,90 @@
 'use strict';
 
 module.exports = (() => {
-	const bunyan = require('bunyan');
-	const fs = require('fs');
+  const bunyan = require('bunyan');
+  const fs = require('fs');
 
-	const isDev = process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'production';
-	const LOG_FOLDER = './logs/';
+  const isDev = process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'production';
+  const LOG_FOLDER = './logs/';
 
-	// Create folder if it doesn't exist.
-	if (!fs.existsSync(LOG_FOLDER) && !isDev)
-		fs.mkdirSync(LOG_FOLDER);
+  // Create folder if it doesn't exist.
+  if (!fs.existsSync(LOG_FOLDER) && !isDev) {
+    fs.mkdirSync(LOG_FOLDER);
+  }
 
-	var streams = [];
+  var streams = [];
 
-	// Log to stdout and stderr when on a development environment.
-	if (isDev) {
-		streams.push(...[{
-			level: 'fatal',
-			stream: process.stderr
-		}, {
-			level: 'info',
-			stream: process.stdout
-		}]);
-	} else {
-		// Log to files when running on test or production environments.
-		streams.push(...[{
-			level: 'error',
-			path: `${LOG_FOLDER}error.json`
-		}, {
-			level: 'warn',
-			path: `${LOG_FOLDER}warn.json`
-		}, {
-			level: 'info',
-			path: `${LOG_FOLDER}info.json`
-		}]);
-	}
+  // Log to stdout and stderr when on a development environment.
+  if (isDev) {
+    streams.push(...[{
+      level: 'fatal',
+      stream: process.stderr
+    }, {
+      level: 'info',
+      stream: process.stdout
+    }]);
+  } else {
+    // Log to files when running on test or production environments.
+    streams.push(...[{
+      level: 'error',
+      path: `${LOG_FOLDER}error.json`
+    }, {
+      level: 'warn',
+      path: `${LOG_FOLDER}warn.json`
+    }, {
+      level: 'info',
+      path: `${LOG_FOLDER}info.json`
+    }]);
+  }
 
-	// TODO: Add the app name as an option when requiring the module.
-	const log = bunyan.createLogger({
-		name: 'muamba',
-		streams: streams
-	});
+  // TODO: Add the app name as an option when requiring the module.
+  const log = bunyan.createLogger({
+    name: 'muamba',
+    streams: streams
+  });
 
-	// TODO: Improve this class.
-	class Trace {
-		constructor() {
-			throw new TypeError('Should not instance static class: Trace.');
-		}
+  // TODO: Improve this class.
+  class Trace {
+    constructor () {
+      throw new TypeError('Should not instance static class: Trace.');
+    }
 
-		static fatal(err, msg) {
-			// Unimplemented.
-		}
+    static fatal () {
+      throw new Error('Unimplemented');
+    }
 
-		static error(err, msg) {
-			if (msg)
-				log.error(err, msg);
-			else
-				log.error(err);
-		}
+    static error (err, msg) {
+      if (msg) {
+        log.error(err, msg);
+      } else {
+        log.error(err);
+      }
+    }
 
-		static warn(msg) {
-			log.warn(msg);
-		}
+    static warn (fields, msg) {
+      if (msg) {
+        log.warn(fields, msg);
+      } else {
+        log.warn(fields);
+      }
+    }
 
-		static info(msg) {
-			log.info(msg);
-		}
+    static info (fields, msg) {
+      if (msg) {
+        log.info(fields, msg);
+      } else {
+        log.info(fields);
+      }
+    }
 
-		static debug(msg) {
-			// Unimplemented.
-		}
+    static debug (msg) {
+      throw new Error('Unimplemented');
+    }
 
-		static trace(msg) {
-			// Unimplemented.
-		}
-	}
+    static trace (msg) {
+      throw new Error('Unimplemented');
+    }
+  }
 
-	return Trace;
+  return Trace;
 })();
